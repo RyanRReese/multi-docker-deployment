c4d5f6484740b15578603a5c83b6ac553f68b253
ryan.holtschneider <ryanreese@comcast.net>
2021-11-12 12:47:05 -0500

    forgot to change ref

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders Learn how to make a website link', () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
